4fdd21215b807893c906b7e2ecd1d6e0d39257f6
Snjallr <snjallr@Mac.netis.cc>
2026-07-13 13:26:24 +0300

    fix android building

--- artifacts/marsool/plugins/withRoomFix.js
+++ artifacts/marsool/plugins/withRoomFix.js
@@ -21,33 +21,33 @@ const resolveConfigPlugins = () => {
   return require('@expo/config-plugins');
 };
 
-const { withAppBuildGradle } = resolveConfigPlugins();
+const { withProjectBuildGradle } = resolveConfigPlugins();
 
 module.exports = function withRoomFix(config) {
-  return withAppBuildGradle(config, (config) => {
+  return withProjectBuildGradle(config, (config) => {
     const contents = config.modResults.contents;
 
     if (!contents) {
       return config;
     }
 
-    const resolutionBlock = `
-configurations.all {
-    resolutionStrategy {
-        force 'androidx.room:room-runtime:2.7.1'
-        force 'androidx.room:room-ktx:2.7.1'
-        force 'androidx.room:room-common:2.7.1'
-    }
-}
-`;
-
     if (contents.includes('room-runtime')) {
       return config;
     }
 
+    const resolutionBlock = `
+    configurations.all {
+        resolutionStrategy {
+            force 'androidx.room:room-runtime:2.7.1'
+            force 'androidx.room:room-ktx:2.7.1'
+            force 'androidx.room:room-common:2.7.1'
+        }
+    }
+`;
+
     config.modResults.contents = contents.replace(
-      /^(android\s*\{)/m,
-      `${resolutionBlock}\n$1`
+      /^(allprojects\s*\{)/m,
+      `$1\n${resolutionBlock}`
     );
 
     return config;
